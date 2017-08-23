c38a7ec8979db87489b5b43f3fa0d769abb7f01d
curran <curran.kelleher@gmail.com>
2017-08-23 15:48:57 +0530

    Establish Dataset. Closes #4

--- data/process.js
+++ data/process.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
-const d3 = require('d3-dsv');
+const d3 = Object.assign(
+  {},
+  require('d3-dsv'),
+  require('d3-collection')
+);
 
 const csvStringRaw = fs.readFileSync('time_series.csv', 'utf8');
-const csvString = csvStringRaw.substr(170, 1000);
+const csvString = csvStringRaw.substr(170);
 
 const csvData = d3.csvParse(csvString);
+
+const codes = {}; // value --> code
+const codeValues = []; // code (an integer) --> value
+const getCode = value => {
+  let code = codes[value];
+  if (code === undefined) {
+    code = codes[value] = codeValues.length;
+    codeValues.push(value);
+  }
+  return code;
+};
 
 // Each data entry looks like this:
 // { Year: '1951',
@@ -15,10 +30,20 @@ const csvData = d3.csvParse(csvString);
 
 const data = csvData.map(d => ({
   year: d.Year,
-  src: d.Origin,
-  dest: d['Country / territory of asylum/residence'],
+  src: getCode(d.Origin),
+  dest: getCode(d['Country / territory of asylum/residence']),
   type: d['Population type'],
   value: +d.Value
 }));
 
-console.log(data[0]);
+const nested = d3.nest()
+  .key(d => d.year)
+  .key(d => d.type)
+  .key(d => d.src)
+  .key(d => d.dest)
+  .rollup(values => values[0].value)
+  .object(data);
+
+const outputData = { codeValues, nested };
+
+fs.writeFileSync('data.json', JSON.stringify(outputData));
