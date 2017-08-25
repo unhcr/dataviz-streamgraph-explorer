4b3815e3a1020e55fd13b68d0223e41b1bd08984
curran <curran.kelleher@gmail.com>
2017-08-25 18:55:13 +0530

    Detect mobile. Closes #13

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Webpack is working");
+function detectMobile() {
+   if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
+const device = detectMobile() ? "mobile" : "desktop";
+console.log("We are on a " + device);
+
