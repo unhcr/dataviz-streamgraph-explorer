0d0a4a8e289de350f3f0c42334e2139f7c34f1a8
curran <curran.kelleher@gmail.com>
2017-08-25 18:56:51 +0530

    Split out detectMobile into module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-function detectMobile() {
-   if(window.innerWidth <= 800 && window.innerHeight <= 600) {
-     return true;
-   } else {
-     return false;
-   }
-}
-
-const device = detectMobile() ? "mobile" : "desktop";
-console.log("We are on a " + device);
+import detectMobile from './detectMobile';
+const device = detectMobile() ? 'mobile' : 'desktop';
+console.log('We are on a ' + device);
 
