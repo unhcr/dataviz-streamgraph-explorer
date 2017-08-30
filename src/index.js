72e52f8f424fce76cb89129287743d3aa7140864
curran <curran.kelleher@gmail.com>
2017-08-30 16:52:47 +0530

    Load and Unpack Data. Closes #19

--- src/index.js
+++ src/index.js
@@ -10,7 +10,36 @@ resize();
 select('#focus svg').style('background-color', 'pink');
 select('#details svg').style('background-color', 'pink');
 
+const unpack = packedData => {
+  const data = [];
+
+  const codeValues = packedData.codeValues;
+  const nested = packedData.nested;
+
+  const years = Object.keys(nested);
+  years.forEach(year => {
+    const byType = nested[year];
+    Object.keys(byType).forEach(type => {
+      const bySrcCode = byType[type];
+      Object.keys(bySrcCode).forEach(srcCode => {
+        const src = codeValues[srcCode];
+        const byDestCode = bySrcCode[srcCode];
+        Object.keys(byDestCode).forEach(destCode => {
+          const dest = codeValues[destCode];
+          const value = byDestCode[destCode];
+
+          data.push({ year, src, dest, type, value });
+
+        });
+      });
+    });
+  });
+
+  return data;
+};
+
 // Load and unpack the data.
 json('data/data.json', packedData => {
-  console.log(packedData);
+  const data = unpack(packedData);
+  console.log(data);
 });
