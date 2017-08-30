5b35c4153f7706cf6d9bfe72f990ab45a8703db6
curran <curran.kelleher@gmail.com>
2017-08-30 14:03:54 +0530

    Add focus and details SVG elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import { select } from 'd3-selection';
 import detectMobile from './detectMobile';
 const device = detectMobile() ? 'mobile' : 'desktop';
 console.log('We are on a ' + device);
+
+select('#focus')
+    .attr('width', 500)
+    .attr('height', 500)
+    .style('background-color', 'pink');
+
+select('#details')
+    .attr('width', 500)
+    .attr('height', 500)
+    .style('background-color', 'pink');
