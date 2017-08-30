cfce23f0039c1da7932299fd60809672dfe33459
curran <curran.kelleher@gmail.com>
2017-08-30 14:36:41 +0530

    Complete SVG resizing behavior for mobile and desktop. Closes #12

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 import { select } from 'd3-selection';
 import detectMobile from './detectMobile';
 
+// These values determine the height of each SVG on mobile devices.
+const focusMobileHeight = 500;
+const detailsMobileHeight = 500;
+
+// Set background color to be pink so we can see the SVGs (temporary).
+select('#focus svg').style('background-color', 'pink');
+select('#details svg').style('background-color', 'pink');
+
+// This function gets called on load and on browser resize.
 function resize() {
 
+  // Detect if we're on desktop or mobile.
   const device = detectMobile() ? 'mobile' : 'desktop';
-  console.log('We are on a ' + device);
 
+  // Get the CSS-computed bounding boxes of the DIVs containing the SVGs.
   const focus = select('#focus').node().getBoundingClientRect();
-  select('#focus svg')
-      .attr('width', focus.width)
-      .attr('height', window.innerHeight - focus.top)
-      .style('background-color', 'pink');
-
   const details = select('#details').node().getBoundingClientRect();
-  select('#details svg')
-      .attr('width', details.width)
-      .attr('height', window.innerHeight - details.top)
-      .style('background-color', 'pink');
+
+  const focusSVG = select('#focus svg');
+  const detailsSVG = select('#details svg');
+
+  // Set the width to fill the DIV, on both mobile and desktop.
+  focusSVG.attr('width', focus.width);
+  detailsSVG.attr('width', details.width);
+
+  // For desktop, set the heights to fill in all the available space.
+  if (device === 'desktop') {
+    focusSVG.attr('height', window.innerHeight - focus.top);
+    detailsSVG.attr('height', window.innerHeight - details.top);
+  }
+  
+  // For mobile, use fixed heights.
+  if (device === 'mobile') {
+    focusSVG.attr('height', focusMobileHeight);
+    detailsSVG.attr('height', detailsMobileHeight);
+  }
 }
 
 
