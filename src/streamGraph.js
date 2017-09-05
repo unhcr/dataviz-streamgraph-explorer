ec3712a9d7b295d003963c2905f8e381de2d6857
curran <curran.kelleher@gmail.com>
2017-09-05 15:58:01 +0530

    Sketch out StreamGraph component, reshape data for StreamGraph consumption.

--- src/streamGraph.js
+++ src/streamGraph.js
@@ -1,5 +1,37 @@
-const StreamGraph = (selection, props) => {
-  console.log(props);
-};
+import { component } from 'd3-component';
+import {
+  area,
+  curveBasis,
+  stack,
+  stackOffsetWiggle,
+  stackOrderInsideOut
+} from 'd3-shape';
+import {
+  scaleTime,
+  scaleLinear,
+  scaleOrdinal,
+  schemeCategory10,
+} from 'd3-scale';
+
+const xValue = d => d.date;
+
+const xScale = scaleTime();
+const yScale = scaleLinear();
+const colorScale = scaleOrdinal().range(schemeCategory10);
+
+const streamArea = area()
+  .x(d => xScale(xValue(d.data)))
+  .y0(d => yScale(d[0]))
+  .y1(d => yScale(d[1]))
+  .curve(curveBasis);
+
+const streamStack = stack()
+  .offset(stackOffsetWiggle)
+  .order(stackOrderInsideOut);
+
+const StreamGraph = component()
+  .render((selection, props) => {
+    console.log(props);
+  });
 
 export default StreamGraph;
