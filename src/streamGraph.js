b504154278c76498685ff093086ea354731a46e1
curran <curran.kelleher@gmail.com>
2017-09-05 16:43:19 +0530

    Refactor, document

--- src/streamGraph.js
+++ src/streamGraph.js
@@ -30,6 +30,7 @@ const streamStack = stack()
   .offset(stackOffsetWiggle)
   .order(stackOrderInsideOut);
 
+// This function computes the keys present in the data.
 const computeKeys = data => {
   const keysSet = set();
   Object.keys(data).forEach(year => {
@@ -40,18 +41,22 @@ const computeKeys = data => {
   return keysSet.values();
 };
 
+// This function preprocesses the data into
+// the data structure that d3.stack can work with.
+const forStacking = data => Object.keys(data)
+  .map(year => {
+    const d = data[year];
+    d.date = d.date || new Date(year);
+    return d;
+  });
+
 const StreamGraph = component()
   .render((selection, props) => {
     const data = props.data;
     const box = props.data;
-    const keys = computeKeys(data);
-    const dataForStacking = Object.keys(data)
-      .map(year => {
-        const d = data[year];
-        d.date = d.date || new Date(year);
-        return d;
-      });
-    const stacked = streamStack.keys(keys)(dataForStacking);
+    const stacked = streamStack
+      .keys(computeKeys(data))
+      (forStacking(data));
 
     console.log(stacked);
   });
