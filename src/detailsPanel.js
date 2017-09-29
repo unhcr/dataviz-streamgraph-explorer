f4d58aebbbbdfee66836e5ba145b2179f1f1b439
curran <curran.kelleher@gmail.com>
2017-09-29 20:10:11 +0530

    Handle (single src, single dest)

--- src/detailsPanel.js
+++ src/detailsPanel.js
@@ -8,12 +8,15 @@ const commaFormat = format(',');
 // Extracts the data for the given year,
 // and transforms it into a sorted array.
 function getYearData(year, data){
-  return Object.keys(data[year])
-    .map(key => ({
-      name: key,
-      value: data[year][key]
-    }))
-    .sort((a, b) => descending(a.value, b.value));
+  if (data[year]) {
+    return Object.keys(data[year])
+      .map(key => ({
+        name: key,
+        value: data[year][key]
+      }))
+      .sort((a, b) => descending(a.value, b.value));
+  }
+  return [];
 }
 
 // Takes the first 20 elements of the data array.
@@ -28,36 +31,45 @@ export default function (selection, year, srcData, destData) {
   const yearDestData = getYearData(year, destData);
 
   // Figure out if there are multipld src/dest.
+  const zeroSrc = yearSrcData.length === 0;
+  const zeroDest = yearDestData.length === 0;
   const multipleSrc = yearSrcData.length > 1;
   const multipleDest = yearDestData.length > 1;
   const singleSrc = !multipleSrc;
   const singleDest = !multipleDest;
 
+  const src = yearSrcData[0];
   const dest = yearDestData[0];
-  const src = yearDestData[0];
 
-  let label = '';
-  let value = NaN;
-  let data = [];
+  let label;
+  let value;
+  let data;
 
   // Handle each of these cases:
+  // - no data (zero)
   // - multiple src, multiple dest
   // - single src, multiple dest
   // - multiple src, single dest
   // - single src, single dest
-  if (multipleSrc && multipleDest) {
+  if (zeroSrc || zeroDest) {
+    label = '';
+    value = '';
+    data = [];
+  } else if (multipleSrc && multipleDest) {
   } else if (singleSrc && multipleDest) {
     label = `Total from ${src.name}`;
-    value = src.value;
+    value = commaFormat(src.value);
     data = yearDestData;
   } else if (multipleSrc && singleDest) {
     label = `Total to ${dest.name}`;
-    value = dest.value;
+    value = commaFormat(dest.value);
     data = yearSrcData;
   } else if (singleSrc && singleDest) {
+    label = `Total from ${src.name} to ${dest.name}`;
+    value = commaFormat(dest.value); // Same as src.value
   }
 
   select('#details-statistic-label').text(label);
-  select('#details-statistic-value').text(commaFormat(value));
+  select('#details-statistic-value').text(value);
   selection.call(detailsBarChart, top20(data));
 };
