53695cc3094bd413c66f3143306c4aae07a72f4c
curran <curran.kelleher@gmail.com>
2017-10-04 17:35:02 +0530

    Got basic context stream to show up

--- src/contextStream.js
+++ src/contextStream.js
@@ -1,10 +1,36 @@
+import { scaleLinear, scaleTime } from 'd3-scale';
+import { area } from 'd3-shape';
+import { component } from 'd3-component';
+import { max, extent } from 'd3-array';
+
+const xValue = d => d.date;
+const xScale = scaleTime();
+
+const yValue = d => d.total;
+const yScale = scaleLinear();
+
+const contextArea = area()
+  .x(d => xScale(xValue(d)))
+  .y0(0)
+  .y1(d => yScale(yValue(d)));
+
+const contextAreaComponent = component('path')
+  .render((selection, props) => {
+    const box = props.box;
+    const data = props.data;
+    xScale
+      .domain(extent(data, xValue))
+      .range([0, box.width]);
+    yScale
+      .domain([0, max(data, yValue)])
+      .range([0, box.height]);
+    selection
+      .attr('transform', `translate(${box.x},${box.y})`)
+      .attr('d', contextArea(data))
+      .attr('fill', 'gray');
+  });
+
 const contextStream = (selection, props) => {
-  console.log(props.data);
-  selection.append('rect')
-    .attr('x', props.box.x)
-    .attr('y', props.box.y)
-    .attr('width', props.box.width)
-    .attr('height', props.box.height)
-    .attr('fill', 'pink');
+  selection.call(contextAreaComponent, props);
 };
 export default contextStream;
